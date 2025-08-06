99b6a5816ebbeddb09b68981d6b9aee4a1acdc83
Death-Ghost <103741754+Death-Ghost@users.noreply.github.com>
2025-08-06 09:35:58 +0200

    CameraView.swift

---
+++ CameraView.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+import AVFoundation
+
+struct CameraView: UIViewRepresentable {
+    let applyFilter: Bool
+
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: UIScreen.main.bounds)
+        let session = AVCaptureSession()
+        session.sessionPreset = .high
+
+        guard
+            let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front),
+            let input = try? AVCaptureDeviceInput(device: device)
+        else { return view }
+
+        session.addInput(input)
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.frame = view.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+
+        session.startRunning()
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
